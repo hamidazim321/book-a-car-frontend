0d3870242607a9678b83a65f669a7cdd84316dc8
hamid321 <hamidazim321@gmail.com>
2024-02-15 13:43:22 +0500

    Add delete car page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import AddCar from './pages/AddCar';
 import Layout from './pages/Layout';
+import DeleteCar from './pages/DeleteCar';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/add-car" element={<AddCar />} />
+        <Route path="/delete-car" element={<DeleteCar />} />
       </Route>
     </Routes>
   );
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,7 +5,7 @@ import NavigationPanel from '../components/NavigationPanel';
 export default function Layout() {
   return (
     <main className="flex">
-      <section className="w-min border-black fixed left-0 top-0 md:static">
+      <section className="w-min">
         <NavigationPanel />
       </section>
       <section className="flex-grow border-black px-2">
