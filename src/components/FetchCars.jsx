5a484c255ca40044ab7514c3ab30c829c50d3521
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-02-19 22:43:18 +0600

    update FetchCars

--- src/components/FetchCars.jsx
+++ src/components/FetchCars.jsx
@@ -1,72 +1,76 @@
-import { Link } from 'react-router-dom';
+/* eslint-disable comma-dangle */
+/* eslint-disable comma-spacing */
+/* eslint-disable quotes */
+import { Link } from "react-router-dom";
 import {
   FaChevronLeft,
   FaChevronRight,
   FaFacebookF,
   FaLinkedinIn,
-} from 'react-icons/fa';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import splitSentence from '../helpers/splitSentence';
+} from "react-icons/fa";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import splitSentence from "../helpers/splitSentence";
 
 const FetchCars = () => {
-    const [cars, setCars] = useState([]);
-    const [currentGroupIndex, setCurrentGroupIndex] = useState(0);
-    const groupSize = 3;
-    const totalGroups = Math.ceil(cars.length / groupSize);
-    const handleNextGroup = () => {
-        setCurrentGroupIndex((prevIndex) => (prevIndex + 1) % totalGroups);
-      };
-    
-      const handlePrevGroup = () => {
-        setCurrentGroupIndex(
-          (prevIndex) => (prevIndex - 1 + totalGroups) % totalGroups
-        );
-      };
-      const displayByThree = cars.slice(
-        currentGroupIndex * groupSize,
-        currentGroupIndex * groupSize + groupSize
-      );
+  const [cars, setCars] = useState([]);
+  const [currentGroupIndex, setCurrentGroupIndex] = useState(0);
+  const groupSize = 3;
+  const totalGroups = Math.ceil(cars.length / groupSize);
 
-      useEffect(() => {
-        const fetchCarsData = async () => {
-          const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-          setCars(res.data);
-        };
-        fetchCarsData();
-      }, []);
+  const handleNextGroup = () => {
+    setCurrentGroupIndex((prevIndex) => (prevIndex + 1) % totalGroups);
+  };
 
-      return (
-        <div className="flex flex-col gap-8 sm:flex-row mt-8">
-          {displayByThree.length > 0 ? (
-            displayByThree.map((car) => (
-              <Link to={`/car-details/${car.id}`} key={car.id} className="card">
-                <div className="avatar">
-                  <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                    <img
-                      src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                      alt="avatar"
-                    />
-                  </div>
+  const handlePrevGroup = () => {
+    setCurrentGroupIndex(
+      (prevIndex) => (prevIndex - 1 + totalGroups) % totalGroups
+    );
+  };
+
+  const displayByThree = cars.slice(
+    currentGroupIndex * groupSize,
+    currentGroupIndex * groupSize + groupSize
+  );
+
+  useEffect(() => {
+    const fetchCarsData = async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setCars(res.data);
+    };
+    fetchCarsData();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-8 sm:flex-row mt-8">
+      {displayByThree.length > 0 ? (
+        displayByThree.map((car) => (
+          <Link to={`/car-details/${car.id}`} key={car.id} className="card">
+            <div className="avatar">
+              <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  alt="avatar"
+                />
+              </div>
+            </div>
+            <div className="card-body">
+              <p>{splitSentence(car.title, 4)}</p>
+              <div className="flex items-center justify-between">
+                <div className="badge badge-outline">
+                  <FaFacebookF />
                 </div>
-                <div className="card-body">
-                  <p>{splitSentence(car.title, 4)}</p>
-                  <div className="flex items-center justify-between">
-                    <div className="badge badge-outline">
-                      <FaFacebookF />
-                    </div>
-                    <div className="badge badge-outline">
-                      <FaLinkedinIn />
-                    </div>
-                  </div>
+                <div className="badge badge-outline">
+                  <FaLinkedinIn />
                 </div>
-              </Link>
-            ))
-          ) : (
-            <p> no Data found</p>
-          )}
-
-<>
+              </div>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <p> no Data found</p>
+      )}
+      <>
         <button
           type="button"
           onClick={handlePrevGroup}
@@ -78,7 +82,6 @@ const FetchCars = () => {
             <span className="sr-only">Previous</span>
           </span>
         </button>
-
         <button
           type="button"
           onClick={handleNextGroup}
@@ -91,8 +94,7 @@ const FetchCars = () => {
         </button>
       </>
     </div>
-
-);
+  );
 };
 
 export default FetchCars;
