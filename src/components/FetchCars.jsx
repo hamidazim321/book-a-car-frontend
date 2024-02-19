e326318a262a1d31369c71884091670963d04805
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-02-19 22:30:30 +0600

    Importing necessary modules and components

---
+++ src/components/FetchCars.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaFacebookF,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import splitSentence from '../helpers/splitSentence';
