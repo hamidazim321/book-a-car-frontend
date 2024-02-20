d88b6c0c225f1570044c1e73edf054c9144f1563
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-02-20 08:34:00 +0600

    Displaying the avatar image of the car.

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -14,4 +14,17 @@ const CarDetails = () => {
       };
       getSingleCar();
     }, []);
-  
+    return (
+        <div className="flex w-full">
+          <div className="w-2/3">
+            <div className="avatar">
+              <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  alt="avatar"
+                />
+              </div>
+            </div>
+          </div>
+)
+}    
