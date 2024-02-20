4412d82235f4854394713f8b23583cac606f3381
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-02-20 08:37:44 +0600

    fix linter error and update

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,32 +1,34 @@
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+
 const CarDetails = () => {
-    const [car, setCar] = useState({});
-    const { carId } = useParams();
-  
-    useEffect(() => {
-      const getSingleCar = async () => {
-        const res = await axios.get(
-          `https://jsonplaceholder.typicode.com/posts/${carId}`,
-        );
-        setCar(res.data);
-      };
-      getSingleCar();
-    }, []);
-    return (
-        <div className="flex w-full">
-          <div className="w-2/3">
-            <div className="avatar">
-              <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img
-                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                  alt="avatar"
-                />
-              </div>
-            </div>
+  const [car, setCar] = useState({});
+  const { carId } = useParams();
+
+  useEffect(() => {
+    const getSingleCar = async () => {
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${carId}`,
+      );
+      setCar(res.data);
+    };
+    getSingleCar();
+  }, []);
+
+  return (
+    <div className="flex w-full">
+      <div className="w-2/3">
+        <div className="avatar">
+          <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img
+              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              alt="avatar"
+            />
           </div>
-          <div className="w-1/3">
+        </div>
+      </div>
+      <div className="w-1/3">
         <div className=" overflow-x-auto">
           <table className="table table-zebra">
             <tbody>
@@ -47,6 +49,4 @@ const CarDetails = () => {
   );
 };
 
-)
-}    
 export default CarDetails;
