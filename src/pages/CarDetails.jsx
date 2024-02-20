530de422f13d7d65c1506fb7b2396f02074d40c4
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-02-20 08:32:41 +0600

    Defining the CarDetails component, setting up state for car details and fetching data using useEffect hook.

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,3 +1,17 @@
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+const CarDetails = () => {
+    const [car, setCar] = useState({});
+    const { carId } = useParams();
+  
+    useEffect(() => {
+      const getSingleCar = async () => {
+        const res = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${carId}`,
+        );
+        setCar(res.data);
+      };
+      getSingleCar();
+    }, []);
+  
