5440a7c63f718eef9152beff95b20e1066fe979e
Md Sazzad Hossain <116729064+SAZZAD1Q2@users.noreply.github.com>
2024-02-20 21:04:47 +0600

    Title for Cars added

--- src/components/FetchCars.jsx
+++ src/components/FetchCars.jsx
@@ -42,58 +42,64 @@ const FetchCars = () => {
   }, []);
 
   return (
-    <div className="flex flex-col gap-8 sm:flex-row mt-8">
-      {displayByThree.length > 0 ? (
-        displayByThree.map((car) => (
-          <Link to={`/car-details/${car.id}`} key={car.id} className="card">
-            <div className="avatar">
-              <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img
-                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                  alt="avatar"
-                />
-              </div>
-            </div>
-            <div className="card-body">
-              <p>{splitSentence(car.title, 4)}</p>
-              <div className="flex items-center justify-between">
-                <div className="badge badge-outline">
-                  <FaFacebookF />
-                </div>
-                <div className="badge badge-outline">
-                  <FaLinkedinIn />
+    /* JSX elements start */
+    <div>
+      <h1 className="text-4xl font-bold text-center my-4">Latest Models</h1>
+      <h2 className="text-lg text-center">Please select a car model.</h2>
+      <div className="flex flex-col gap-8 sm:flex-row mt-8">
+        {displayByThree.length > 0 ? (
+          displayByThree.map((car) => (
+            <Link to={`/car-details/${car.id}`} key={car.id} className="card">
+              <div className="avatar">
+                <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img
+                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    alt="avatar"
+                  />
                 </div>
               </div>
-            </div>
-          </Link>
-        ))
-      ) : (
-        <p> no Data found</p>
-      )}
-      <>
-        <button
-          type="button"
-          onClick={handlePrevGroup}
-          className="absolute top-0 -left-15 z-30 flex items-center justify-center h-full px-4 cursor-pointer"
-        >
-          <span className="inline-flex btn btn-primary bg-lime-500 items-center justify-center w-10 h-10 hover:bg-lime-500">
-            <FaChevronLeft />
-            {' '}
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          onClick={handleNextGroup}
-          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        >
-          <span className="inline-flex btn btn-primary bg-lime-500 items-center justify-center w-10 h-10 hover:bg-lime-500">
-            <FaChevronRight />
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </>
+              <div className="card-body">
+                <p>{splitSentence(car.title, 4)}</p>
+                <div className="flex items-center justify-between">
+                  <div className="badge badge-outline">
+                    <FaFacebookF />
+                  </div>
+                  <div className="badge badge-outline">
+                    <FaLinkedinIn />
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <p> no Data found</p>
+        )}
+        <>
+          <button
+            type="button"
+            onClick={handlePrevGroup}
+            className="absolute top-0 -left-15 z-30 flex items-center justify-center h-full px-4 cursor-pointer"
+          >
+            <span className="inline-flex btn btn-primary bg-lime-500 items-center justify-center w-10 h-10 hover:bg-lime-500">
+              <FaChevronLeft />
+              {' '}
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+          <button
+            type="button"
+            onClick={handleNextGroup}
+            className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          >
+            <span className="inline-flex btn btn-primary bg-lime-500 items-center justify-center w-10 h-10 hover:bg-lime-500">
+              <FaChevronRight />
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
+        </>
+      </div>
     </div>
+    /* JSX elements end */
   );
 };
 
