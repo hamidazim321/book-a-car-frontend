71acfe7ae4f27fe943e42046021a9a6e1df38661
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-02-26 08:09:10 +0200

    Generalize toast functionality

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,40 +1,44 @@
 import React, { useEffect } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router';
 import NavigationPanel from '../components/NavigationPanel';
-import { clearToast } from '../redux/toast/toastSlice';
 
 export default function Layout() {
-  const { error, success, info } = useSelector((state) => state.toast);
-  const dispatch = useDispatch();
+  const carState = useSelector((state) => state.car);
+  const reservationState = useSelector((state) => state.reservations);
+  const authState = useSelector((state) => state.auth);
+
   useEffect(() => {
+    const error = authState.error || reservationState.error || carState.error;
+    const success = authState.success || reservationState.success || carState.success;
+    const info = authState.info || reservationState.info || carState.info;
     if (error) {
       toast.error(`Error! ${error}`, {
         position: 'top-center',
       });
-      dispatch(clearToast());
     }
-  }, [dispatch, error]);
-
-  useEffect(() => {
     if (success) {
       toast.success(`Success! ${success}`, {
         position: 'top-center',
       });
-      dispatch(clearToast());
     }
-  }, [dispatch, success]);
-
-  useEffect(() => {
     if (info) {
       toast.info(`Info! ${info}`, {
         position: 'top-center',
       });
-      dispatch(clearToast());
     }
-  }, [dispatch, info]);
+  }, [carState.error,
+    reservationState.error,
+    authState.error,
+    authState.success,
+    reservationState.success,
+    carState.success,
+    authState.info,
+    reservationState.info,
+    carState.info]);
+
   return (
     <main className="flex">
       <ToastContainer />
