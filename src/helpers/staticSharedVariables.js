7f6832852d7aaa1dacf8825da20ad2f64516a56e
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-02-27 10:52:25 +0200

    Move majorCities array to static vars

--- src/helpers/staticSharedVariables.js
+++ src/helpers/staticSharedVariables.js
@@ -1,3 +1,16 @@
 const APIURL = () => (process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api/v1' : 'https://book-a-car.onrender.com');
 
+export const majorCities = [
+  'New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix',
+  'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose',
+  'Austin', 'Jacksonville', 'San Francisco', 'Columbus', 'Fort Worth',
+  'Indianapolis', 'Charlotte', 'Seattle', 'Denver', 'Washington',
+  'Boston', 'El Paso', 'Nashville', 'Detroit', 'Oklahoma City',
+  'Portland', 'Las Vegas', 'Memphis', 'Louisville', 'Baltimore',
+  'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Mesa',
+  'Sacramento', 'Atlanta', 'Kansas City', 'Colorado Springs', 'Omaha',
+  'Raleigh', 'Miami', 'Long Beach', 'Virginia Beach', 'Oakland',
+  'Minneapolis', 'Tulsa', 'Arlington', 'Tampa', 'New Orleans',
+];
+
 export default APIURL;
